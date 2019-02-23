ebcab5bf7c48ba799971f2b21dd3a20c1eaabffa
Michelle Kershaw <kershawmom@gmail.com>
2019-02-23 13:25:33 -0700

    keylogger

--- keylogger/src/Main/Main.jsx
+++ keylogger/src/Main/Main.jsx
@@ -7,11 +7,17 @@ class Main extends Component {
 
     state = {
         textValue: '',
-
+        listText: [],
+        list: ['Street', 'Sportster', 'Dyna', 'SoftTail', 'V-Rod', 'Touring', 'CVO', 'Trike', 'Limited']
     }
+
     handleChange = (event) => this.setState({
         textValue: event.target.value
+
     })
+
+    listItems = this.state.list.map((item) =>
+        <li> {item}</li>)
 
     render() {
         return (
@@ -19,24 +25,31 @@ class Main extends Component {
 
                 <h1> Keylogger </h1>
                 <div>
-
                     <span> Input Text:</span>
                     <input type="text" onChange={this.handleChange} />
-
-
+                    <br></br>
                 </div>
                 <div>
                     <span> Value:</span>
                     <span>{this.state.textValue}</span>
                 </div>
+                <br></br>
+
+                <div>
+                    <div id="arraylist" class="newExercise">
+                        <h1> Array List </h1>
 
 
+                        <ul> I Love Harley Motorcycles:
+
+
+                {this.state.list.map((item) =>
+                            <li> {item}</li>)}
+                        </ul>
+                    </div>
+                </div>
             </div>
-
-
         )
-
-
 
     }
 }
